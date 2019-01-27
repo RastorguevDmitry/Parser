7a88274361cc38ece0e65da159a94e88bb7f3ad9
Dmitry <rdi81@yandex.ru>
2019-01-27 18:30:41 +0300

    доработка сохранения в файл, название файла, папки

--- main/papka/Parser.java
+++ main/papka/Parser.java
@@ -15,6 +15,10 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -24,55 +28,55 @@ public class Parser {
 
     private static Document getPage(int str) throws IOException {
 
-      // String url = "http://www.pogoda.spb.ru/";
-        String url1 =  "http://zakupki.gov.ru/223/plan/public/plan/info/positions.html?planInfoId=2897106&planId=377214&d-5492750-p=";
+        // String url = "http://www.pogoda.spb.ru/";
+        String url1 = "http://zakupki.gov.ru/223/plan/public/plan/info/positions.html?planInfoId=2897106&planId=377214&d-5492750-p=";
         String url2 = "&versioned=&activeTab=4&epz=true";
 
-        Document page = Jsoup.parse(new URL(url1+str+url2),60000);
+        Document page = Jsoup.parse(new URL(url1 + str + url2), 60000);
         return page;
     }
 
 
-            private  static Pattern patternPoiskaNomeraPZ = Pattern.compile("\\b\\d{9}\\b"); // регулярные выражения
+    private static Pattern patternPoiskaNomeraPZ = Pattern.compile("\\b\\d{9}\\b"); // регулярные выражения
 
-            private static String getDateFromString(String stringDate) throws Exception{
-            Matcher matcher = patternPoiskaNomeraPZ.matcher(stringDate);
-            if (matcher.find()){
-                return matcher.group();
-            }
-            throw new Exception("Cant extract");
-              }
+    private static String getDateFromString(String stringDate) throws Exception {
+        Matcher matcher = patternPoiskaNomeraPZ.matcher(stringDate);
+        if (matcher.find()) {
+            return matcher.group();
+        }
+        throw new Exception("Cant extract");
+    }
 
-            private  static Pattern patternPoiskaNomeraZakupkiEIS = Pattern.compile("\\d{11}"); // регулярные выражения
+    private static Pattern patternPoiskaNomeraZakupkiEIS = Pattern.compile("\\d{11}"); // регулярные выражения
 
 
-            private static String getNomerZakupkiEISFromString(String stringDate) throws Exception{
-                Matcher matcher = patternPoiskaNomeraZakupkiEIS.matcher(stringDate);
-                if (matcher.find()){
-                    return matcher.group();
-                }
-                return "0";
-                //throw new Exception("Cant extract");
-            }
+    private static String getNomerZakupkiEISFromString(String stringDate) throws Exception {
+        Matcher matcher = patternPoiskaNomeraZakupkiEIS.matcher(stringDate);
+        if (matcher.find()) {
+            return matcher.group();
+        }
+        return "0";
+        //throw new Exception("Cant extract");
+    }
 
-    private static int printFourValues(Elements values, int index){
+    private static int printFourValues(Elements values, int index) {
         int iterationCount = 4;
-        for(int i = 0; i < iterationCount; i++){
+        for (int i = 0; i < iterationCount; i++) {
             Element valueLine = values.get(index + i);
-            for (Element td: valueLine.select("td")){
+            for (Element td : valueLine.select("td")) {
                 System.out.print(td.text() + "    ");
             }
             System.out.println();
         }
 
 
-        return  iterationCount;
+        return iterationCount;
     }
 
-    public static void main (String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-
-        for (int str = 1; str < 5; str++) {
+        List<Employee> list = new ArrayList<Employee>();
+        for (int str = 1; str <= 2; str++) {
 
             Document page = getPage(str);
             Element spisokZakupokNaStraniche = page.select("tbody").get(3);
@@ -81,6 +85,7 @@ public class Parser {
 
             // Elements values = tablePosition.select("tr[valign=top]");
             int index = 0;
+
             for (Element name : zakupokiSClassomODD) {      //перебираем закупки
                 // String dateString = name.select("td[style=text-align:left; width:60px;]").text();
                 // String nomerPZ = getDateFromString (dateString);
@@ -103,17 +108,20 @@ public class Parser {
                 String dopolnitelnayaInformachiya = td.get(5).text();
                 String NomerZakupkiEIS = getNomerZakupkiEISFromString(dopolnitelnayaInformachiya);
 
-                System.out.println(nomerPZ + "  " + NomerZakupkiEIS);
+                Employee e1 = new Employee(nomerPZ, NomerZakupkiEIS, nazvanieLota, nachalnayaMaxCenalota, srokIspolneniyaDogovora);
+                list.add(e1);
+
+
+               // System.out.println(nomerPZ + "  " + NomerZakupkiEIS);
 
             }
         }
 
 
-
         HSSFWorkbook workbook = new HSSFWorkbook();
-        HSSFSheet sheet = workbook.createSheet("Employees sheet");
+        HSSFSheet sheet = workbook.createSheet("Список номеров ПЗ из ЕИС");
 
-        List<Employee> list = EmployeeDAO.listEmployees();
+       // List<Employee> list = EmployeeDAO.listEmployees();
 
         int rownum = 0;
         Cell cell;
@@ -121,27 +129,27 @@ public class Parser {
         //
         HSSFCellStyle style = createStyleForTitle(workbook);
 
-    row = sheet.createRow(rownum);
+        row = sheet.createRow(rownum);
 
-    // EmpNo
-    cell = row.createCell(0, CellType.STRING);
-        cell.setCellValue("EmpNo");
+        // EmpNo
+        cell = row.createCell(0, CellType.STRING);
+        cell.setCellValue("Номер закупки из плана закупки ЕИС");
         cell.setCellStyle(style);
-    // EmpName
-    cell = row.createCell(1, CellType.STRING);
-        cell.setCellValue("EmpNo");
+        // EmpName
+        cell = row.createCell(1, CellType.STRING);
+        cell.setCellValue("Номер измещения ЕИС");
         cell.setCellStyle(style);
-    // Salary
-    cell = row.createCell(2, CellType.STRING);
-        cell.setCellValue("Salary");
+        // Salary
+        cell = row.createCell(2, CellType.STRING);
+        cell.setCellValue("Название лота");
         cell.setCellStyle(style);
-    // Grade
-    cell = row.createCell(3, CellType.STRING);
-        cell.setCellValue("Grade");
+        // Grade
+        cell = row.createCell(3, CellType.STRING);
+        cell.setCellValue("НАЧАЛЬНАЯ МАКСИМАЛЬНАЯ ЦЕНА ДОГОВОРА");
         cell.setCellStyle(style);
-    // Bonus
-    cell = row.createCell(4, CellType.STRING);
-        cell.setCellValue("Bonus");
+        // Bonus
+        cell = row.createCell(4, CellType.STRING);
+        cell.setCellValue("СРОК ИСПОЛНЕНИЯ ДОГОВОРА");
         cell.setCellStyle(style);
 
         // Data
@@ -156,17 +164,25 @@ public class Parser {
             cell = row.createCell(1, CellType.STRING);
             cell.setCellValue(emp.getEmpName());
             // Salary (C)
-            cell = row.createCell(2, CellType.NUMERIC);
+            cell = row.createCell(2, CellType.STRING);
             cell.setCellValue(emp.getSalary());
             // Grade (D)
-            cell = row.createCell(3, CellType.NUMERIC);
-            cell.setCellValue(emp.getGrade());
+            cell = row.createCell(3, CellType.STRING);
+            cell.setCellValue(emp.getNachalnayaMaxCenalota());
             // Bonus (E)
-            String formula = "0.1*C" + (rownum + 1) + "*D" + (rownum + 1);
-            cell = row.createCell(4, CellType.FORMULA);
-            cell.setCellFormula(formula);
+            cell = row.createCell(4, CellType.STRING);
+            cell.setCellValue(emp.getSrokIspolneniyaDogovora());
+
+//            String formula = "0.1*C" + (rownum + 1) + "*D" + (rownum + 1);
+//            cell = row.createCell(4, CellType.FORMULA);
+//            cell.setCellFormula(formula);
         }
-        File file = new File("D:/RDI/employee.xls"); //D:\RDI
+
+
+        DateFormat dateFormat = new SimpleDateFormat("yy-MM-dd_HH-mm-s"); // формат для названия файла
+        Date date = new Date();
+
+        File file = new File("D:/RDI/FileSkachivaniya/SpisokPZ-NomeraEIS" + dateFormat.format(date) + ".xls");
         file.getParentFile().mkdirs();
 
         FileOutputStream outFile = new FileOutputStream(file);
@@ -182,8 +198,6 @@ public class Parser {
         style.setFont(font);
         return style;
     }
-
-
 
 
 }
